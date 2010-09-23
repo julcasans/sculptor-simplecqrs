89660b319a6093b1b613d02e5f38eaeab14131a7
patriknw <patrik.nordwall@gmail.com>
2010-09-23 21:23:00 +0200

    Added test for pub/sub

--- src/test/java/org/sample/simplecqrs/command/serviceapi/InventoryItemEventPublisherTest.java
+++ src/test/java/org/sample/simplecqrs/command/serviceapi/InventoryItemEventPublisherTest.java
@@ -1,62 +1,55 @@
 package org.sample.simplecqrs.command.serviceapi;
 
-import org.fornax.cartridges.sculptor.framework.accessimpl.mongodb.DbManager;
+import java.util.Date;
 
+import org.fornax.cartridges.sculptor.framework.event.Event;
+import org.fornax.cartridges.sculptor.framework.event.EventBus;
+import org.fornax.cartridges.sculptor.framework.event.EventSubscriber;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import org.sample.simplecqrs.command.domain.InventoryItemRenamed;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 import org.springframework.test.context.ContextConfiguration;
-
-import java.util.Set;
 
 /**
  * Spring based test with MongoDB.
  */
 @RunWith(org.springframework.test.context.junit4.SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations =  {
-    "classpath:applicationContext-test.xml"}
-)
-public class InventoryItemEventPublisherTest
-    extends AbstractDependencyInjectionSpringContextTests
-    implements InventoryItemEventPublisherTestBase {
-    @Autowired
-    private DbManager dbManager;
+@ContextConfiguration(locations = { "classpath:applicationContext-test.xml" })
+public class InventoryItemEventPublisherTest extends AbstractDependencyInjectionSpringContextTests implements
+        InventoryItemEventPublisherTestBase, EventSubscriber {
     @Autowired
     private InventoryItemEventPublisher inventoryItemEventPublisher;
+    @Autowired
+    @Qualifier("eventBus")
+    private EventBus eventBus;
+    private Event eventReceived;
 
     @Before
-    public void initTestData() {
-    }
-
-    @Before
-    public void initDbManagerThreadInstance() throws Exception {
-        // to be able to do lazy loading of associations inside test class
-        DbManager.setThreadInstance(dbManager);
+    public void setUpSubscriber() {
+        eventReceived = null;
+        eventBus.subscribe("inventoryItemTopic", this);
     }
 
     @After
-    public void dropDatabase() {
-        Set<String> names = dbManager.getDB().getCollectionNames();
-        for (String each : names) {
-            if (!each.startsWith("system")) {
-                dbManager.getDB().getCollection(each).drop();
-            }
-        }
-
-        // dbManager.getDB().dropDatabase();
-    }
-
-    private int countRowsInDBCollection(String name) {
-        return (int) dbManager.getDBCollection(name).getCount();
+    public void tearDownSubscriber() {
+        eventReceived = null;
+        eventBus.unsubscribe("inventoryItemTopic", this);
     }
 
     @Test
     public void testPublishEvent() throws Exception {
-        // TODO Auto-generated method stub
-        fail("testPublishEvent not implemented");
+
+        InventoryItemRenamed event = new InventoryItemRenamed(new Date(), "6677", "Something else");
+        inventoryItemEventPublisher.publishEvent(event);
+        assertEquals(event, eventReceived);
+    }
+
+    public void receive(Event event) {
+        eventReceived = event;
     }
 }
