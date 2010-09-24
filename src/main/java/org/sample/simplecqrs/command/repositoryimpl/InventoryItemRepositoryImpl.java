c2a5de94ad71b1d3562b20feb43fd2feb5ac43f6
patriknw <patrik.nordwall@gmail.com>
2010-09-24 11:50:20 +0200

    simplified publish, and cleanup

--- src/main/java/org/sample/simplecqrs/command/repositoryimpl/InventoryItemRepositoryImpl.java
+++ src/main/java/org/sample/simplecqrs/command/repositoryimpl/InventoryItemRepositoryImpl.java
@@ -6,8 +6,6 @@ import org.sample.simplecqrs.command.domain.InventoryItem;
 import org.sample.simplecqrs.command.domain.InventoryItemEvent;
 import org.sample.simplecqrs.command.domain.InventoryItemSnapshot;
 import org.sample.simplecqrs.command.exception.InventoryItemNotFoundException;
-import org.sample.simplecqrs.command.serviceapi.InventoryItemEventPublisher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -16,47 +14,49 @@ import org.springframework.stereotype.Repository;
 @Repository("inventoryItemRepository")
 public class InventoryItemRepositoryImpl extends InventoryItemRepositoryBase {
 
-    @Autowired
-    private InventoryItemEventPublisher inventoryItemEventPublisher;
-
     public InventoryItemRepositoryImpl() {
     }
 
     @Override
-    public InventoryItem save(InventoryItem item) {
-        InventoryItem saved = super.save(item);
+    public InventoryItem save(InventoryItem entity) {
+        InventoryItem saved = super.save(entity);
 
-        List<InventoryItemEvent> changes = item.getUncommittedChanges();
-        long version = saved.getVersion();
+        List<InventoryItemEvent> changes = entity.getUncommittedChanges();
+        assignVersionToChanges(changes, saved.getVersion());
+        for (InventoryItemEvent each : changes) {
+            getInventoryItemEventRepository().save(each);
+        }
+        entity.markChangesAsCommitted();
+
+        return saved;
+    }
+
+    private void assignVersionToChanges(List<InventoryItemEvent> changes, long version) {
         long sequence = version * 1000;
         for (InventoryItemEvent each : changes) {
             each.setVersion(version);
             each.setSeq(sequence);
             sequence++;
         }
-        getInventoryItemEventRepository().save(changes);
-
-        for (InventoryItemEvent each : changes) {
-            inventoryItemEventPublisher.publishEvent(each);
-        }
-
-        item.markChangesAsCommitted();
-
-        return item;
     }
 
     @Override
     public InventoryItem findByKey(String itemId) throws InventoryItemNotFoundException {
         InventoryItem result = super.findByKey(itemId);
 
-        InventoryItemSnapshot snapshot = getInventoryItemSnapshotRepository().getLatestSnapshot(itemId);
-        result.applySnapshot(snapshot);
-        long snapshotVersion = snapshot == null ? 0 : snapshot.getVersion();
-
-        List<InventoryItemEvent> history = getInventoryItemEventRepository().findAllAfter(itemId, snapshotVersion);
-        result.loadFromHistory(history);
+        loadFromHistory(result);
 
         return result;
+    }
+
+    private void loadFromHistory(InventoryItem entity) {
+        InventoryItemSnapshot snapshot = getInventoryItemSnapshotRepository().getLatestSnapshot(entity.getItemId());
+        entity.applySnapshot(snapshot);
+        long snapshotVersion = snapshot == null ? 0 : snapshot.getVersion();
+
+        List<InventoryItemEvent> history = getInventoryItemEventRepository().findAllAfter(entity.getItemId(),
+                snapshotVersion);
+        entity.loadFromHistory(history);
     }
 
 }
